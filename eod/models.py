99cb9b8b6b8b4003563064d930a0c4df09d5888a
Ajay Divakaran <ajay_886@hotmail.com>
2017-01-08 15:29:42 +0530

    Added unique constraint to models

--- eod/models.py
+++ eod/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Contributor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    display_name = models.CharField(max_length=30)
+    display_name = models.CharField(max_length=30, unique=True)
     is_active = models.BooleanField(default=True)
     team = models.ForeignKey(Team)
 
@@ -34,3 +37,6 @@ class DispatchRule(models.Model):
     type = models.CharField(max_length=30)
     hour = models.IntegerField()
     minute = models.IntegerField()
+
+    def __str__(self):
+        return "Rule %s: hour: %s minute: %s" % (self.type, self.hour, self.minute)
