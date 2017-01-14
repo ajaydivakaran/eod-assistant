9adf5454b12043fe7331f546d2759a7fdb7d653a
Ajay Divakaran <ajay_886@hotmail.com>
2017-01-14 15:25:27 +0530

    Added smtp container and settings

--- eodassistant/settings.py
+++ eodassistant/settings.py
@@ -126,4 +126,8 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '25'))
+EMAIL_HOST_USER = os.getenv('EMAIL_USER', 'mailuser')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD', 'password123')
