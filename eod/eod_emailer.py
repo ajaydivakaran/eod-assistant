80c0a5c5907036bc40c52100163f2dc345ae679d
Ajay Divakaran <ajay_886@hotmail.com>
2017-01-14 14:37:35 +0530

    Added email tests

--- eod/eod_emailer.py
+++ eod/eod_emailer.py
@@ -20,6 +20,7 @@ def _send_eod_emails(teams_due_for_eod_mail_dispatch):
     eod_items = get_not_sent_eod_items_for_teams(teams_due_for_eod_mail_dispatch)
     if not eod_items:
         logger.info('No eod items due for dispatch')
+        return
     for team_name, grouped_eod_items in itertools.groupby(eod_items, lambda eod_item: eod_item.team.name):
         team = _get_team_with_name(teams_due_for_eod_mail_dispatch, team_name)
         eod_item_list = list(grouped_eod_items)
---
+++ eod/tests/test_eod_emailer.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+from unittest.mock import patch, MagicMock
+
+from django.contrib.auth.models import User
+from django.core import mail
+from django.test import TestCase
+
+from eod.eod_emailer import send_pending_eod_mails_for_team, send_rule_based_eod_mails
+from eod.models import Team, Contributor
+
+
+class EodEmailerTestCase(TestCase):
+    @patch('eod.eod_emailer.get_active_teams')
+    @patch('eod.eod_emailer.get_logger', )
+    @patch('eod.eod_emailer.get_not_sent_eod_items_for_teams')
+    def test_should_not_send_end_of_day_email_when_no_new_eod_items_present(self, get_not_sent_eod_items_for_teams_mock,
+                                                                            logger_mock,
+                                                                            get_active_teams_mock):
+        fake_user = User(username='user1')
+        logger_instance_mock = MagicMock()
+        logger_mock.return_value = logger_instance_mock
+        team1 = Team(name='team1', email='team1@mailinator.com', user=fake_user)
+        team2 = Team(name='team2', email='team2@mailinator.com', user=fake_user)
+        get_active_teams_mock.return_value = [team1, team2]
+        get_not_sent_eod_items_for_teams_mock.return_value = []
+
+        send_pending_eod_mails_for_team(team_name='team_1')
+
+        self.assertEqual(len(mail.outbox), 0)
+        self.assertEqual(get_active_teams_mock.call_args, ())
+        get_active_teams_mock.assert_called_with()
+        logger_instance_mock.info.assert_called_with('No eod items due for dispatch')
+
+    @patch('eod.eod_emailer.get_active_teams')
+    @patch('eod.eod_emailer.get_logger', )
+    @patch('eod.eod_emailer.get_not_sent_eod_items_for_teams')
+    def test_should_send_end_of_day_email_to_team_when_new_eod_items_present(self,
+                                                                             get_not_sent_eod_items_for_teams_mock,
+                                                                             logger_mock,
+                                                                             get_active_teams_mock):
+        logger_instance_mock = MagicMock()
+        logger_mock.return_value = logger_instance_mock
+        team1 = MagicMock()
+        team1.name = 'team1'
+        team1.email = 'team1@mailinator.com'
+        team2 = MagicMock()
+        team2.name = 'team2'
+        team2.email = 'team2@mailinator.com'
+        c1 = Contributor(first_name='John', last_name='Doe')
+        c2 = Contributor(first_name='Jane', last_name='Doe')
+        get_active_teams_mock.return_value = [team1, team2]
+        e1 = MagicMock(description='desc1', story_id='story_id1', status='status1', team=team1, contributors=[c1, c2])
+        e2 = MagicMock(description='desc2', story_id='story_id2', status='status2', team=team1, contributors=[c2])
+
+        get_not_sent_eod_items_for_teams_mock.return_value = [e1, e2]
+
+        send_pending_eod_mails_for_team(team_name='team1')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Eod mail')
+        self.assertEqual(mail.outbox[0].body, '2 items to be sent')
+        self.assertEqual(mail.outbox[0].from_email, 'no-reply@eodassistant.com')
+        self.assertEqual(mail.outbox[0].recipients(), ['team1@mailinator.com'])
+        logger_instance_mock.info.assert_called_with('Sent mail to team1@mailinator.com with 2 items')
+
+    @patch('eod.eod_emailer.datetime')
+    @patch('eod.eod_emailer.get_teams_with_matching_rule')
+    @patch('eod.eod_emailer.get_logger', )
+    @patch('eod.eod_emailer.get_not_sent_eod_items_for_teams')
+    def test_should_send_end_of_day_email_to_team_with_matching_rule_when_new_eod_items_present(self,
+                                                                                                get_new_eod_items_mock,
+                                                                                                logger_mock,
+                                                                                                get_teams_with_matching_rule,
+                                                                                                datetime_mock):
+        logger_instance_mock = MagicMock()
+        logger_mock.return_value = logger_instance_mock
+        team1 = MagicMock()
+        team1.name = 'team1'
+        team1.email = 'team1@mailinator.com'
+        team2 = MagicMock()
+        team2.name = 'team2'
+        team2.email = 'team2@mailinator.com'
+        c1 = Contributor(first_name='John', last_name='Doe')
+        c2 = Contributor(first_name='Jane', last_name='Doe')
+        get_teams_with_matching_rule.return_value = [team1]
+        e1 = MagicMock(description='desc1', story_id='story_id1', status='status1', team=team1, contributors=[c1, c2])
+        e2 = MagicMock(description='desc2', story_id='story_id2', status='status2', team=team1, contributors=[c2])
+        datetime_mock.now.return_value = datetime(2017, 1, 1, 10, 20, 30)
+        get_new_eod_items_mock.return_value = [e1, e2]
+
+        send_rule_based_eod_mails()
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Eod mail')
+        self.assertEqual(mail.outbox[0].body, '2 items to be sent')
+        self.assertEqual(mail.outbox[0].from_email, 'no-reply@eodassistant.com')
+        self.assertEqual(mail.outbox[0].recipients(), ['team1@mailinator.com'])
+        logger_instance_mock.info.assert_called_with('Sent mail to team1@mailinator.com with 2 items')
+        get_teams_with_matching_rule.assert_called_with(datetime(2017, 1, 1, 10, 20, 30), 'FIXED_TIME_EOD_MAIL')
