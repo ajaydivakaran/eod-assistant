439170210aac6245a5ba348fb6256b8b47ae6dc6
Ajay Divakaran <ajay_886@hotmail.com>
2017-01-15 01:42:52 +0530

    Show only eod for those teams the user is associated with

--- eod/views.py
+++ eod/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Team, Contributor, EndOfDayItem
 from .permissions import StaffWritePermission, TeamEditOnly
@@ -27,8 +28,13 @@ class ContributorViewSet(viewsets.ModelViewSet):
 
 
 class EndOfDayItemViewSet(viewsets.ModelViewSet):
-    permission_classes = (TeamEditOnly,)
+    permission_classes = (TeamEditOnly, IsAuthenticated)
     queryset = EndOfDayItem.objects.all()
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return EndOfDayItem.objects.all()
+        return EndOfDayItem.objects.filter(team__user=self.request.user)
 
     def get_serializer_class(self):
         if self.request.content_type == HTML_CONTENT_TYPE or self.request.method == GET_METHOD:
